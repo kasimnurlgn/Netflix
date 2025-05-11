16672da406282697566856b96f9d976a0e95de91
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-05-11 10:25:38 +0300

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
