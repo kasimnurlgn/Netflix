e310ba9b678e04c799deb5c913d0ca274c2036c2
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-05-12 10:52:08 +0300

    banner jsx added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
+import Layout from "../Layout/Layout";
 const Home = () => {
   return (
     <>
-      <Header />
-      <Footer />
+      <Layout>
+        <Banner />
+      </Layout>
     </>
   );
 };
